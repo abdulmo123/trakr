4ebc7fba089811d1d04e60f6d141a5451c1e294a
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-07-25 18:57:10 -0500

    added route for inserting to workout_exercises table

--- trakr-be/routes/routes.py
+++ trakr-be/routes/routes.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify, request
-from services.services import create_exercise, get_all_exercises, get_all_workouts, insert_workout
-
+from services.services import create_exercise, get_all_exercises, get_all_workouts, insert_workout, log_workout_exercises
 api = Blueprint('api', __name__)
 
 @api.route('/exercises', methods=['GET'])
@@ -64,3 +63,29 @@ def insert_workouts():
         "workout_type": workout.workout_type,
         "notes": workout.notes
     }), 201
+
+@api.route("/workout_exercises", methods=["POST"])
+def log_workout_exercises():
+    data = request.get_json()
+    workout_id = data.get('workout_id')
+    exercise_id = data.get('exercise_id')
+    sets = data.get('sets')
+    reps = data.get('reps')
+    weight = data.get('weight')
+
+    if not workout_id or exercise_id:
+        return jsonify({
+            "error": "Workout and/or Exercise not given!"
+        })
+    
+    workout_exercise = log_workout_exercises(
+        workout_id, exercise_id, sets, reps, weight
+    )
+
+    return jsonify({
+        "workout_id": workout_exercise.workout_id,
+        "exercise_id": workout_exercise.exercise_id,
+        "sets": workout_exercise.sets,
+        "reps": workout_exercise.reps,
+        "weight": workout_exercise.weight
+    })
